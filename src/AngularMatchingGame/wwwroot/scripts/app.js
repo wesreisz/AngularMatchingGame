b16282d6ed033780223141b0c17c01028366f241
Wesley Reisz <wes@wesleyreisz.com>
2015-02-10 21:18:49 -0500

    added simple css layout for board

--- src/AngularMatchingGame/wwwroot/scripts/app.js
+++ src/AngularMatchingGame/wwwroot/scripts/app.js
@@ -3,5 +3,6 @@
 	app.controller('GameController', function () {
 		var myApp = this;
 		myApp.title = "This is my Angular Title";
+		myApp.tileNames = ['8-ball', 'baked-potato', 'dinosaur', 'kronos', 'rocket', 'that-guy', 'zeppelin', 'cards'];
 	});
 })();
