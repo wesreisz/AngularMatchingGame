9182a8a547a9420dbd0d5c2a586335fc41bc3ef1
Wesley Reisz <wes@wesleyreisz.com>
2015-02-10 20:25:06 -0500

    initial angular project structure

---
+++ src/AngularMatchingGame/wwwroot/scripts/app.js
@@ -0,0 +1,7 @@
+﻿(function () {
+	var app = angular.module('app', []);
+	app.controller('GameController', function () {
+		var myApp = this;
+		myApp.title = "This is my Angular Title";
+	});
+})();
