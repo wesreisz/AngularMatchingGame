044695aced574dd9315cb2c199df0bb18a3012fb
Wesley Reisz <wes@wesleyreisz.com>
2015-02-10 21:56:24 -0500

    built initial deck

--- src/AngularMatchingGame/wwwroot/scripts/game.js
+++ src/AngularMatchingGame/wwwroot/scripts/game.js
@@ -1,5 +1,21 @@
 ﻿function Game(title) {
 	var _TILENAMES = ['8-ball', 'baked-potato', 'dinosaur', 'kronos', 'rocket', 'that-guy', 'zeppelin', 'cards'];
 	this.title = title;
-	this.tileDeck = _TILENAMES;
+	this.tileDeck = makeDeck(_TILENAMES);
+
+	function makeDeck(tileNames) {
+		var tiles = [];
+		tileNames.forEach(function (tile) {
+			tiles.push(new Tile(tile));
+			tiles.push(new Tile(tile));
+		});
+		return tiles;
+	};
+}
+function Tile(title) {
+	this.tileTitle = title;
+	this.flipped = false;
+	this.flip = function () {
+		this.flipped = !this.flipped;
+	}
 }
