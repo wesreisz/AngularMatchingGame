5de47c649985542d8c322b264221098dbeb78ea1
Wesley Reisz <wes@wesleyreisz.com>
2015-02-10 23:30:44 -0500

    saving web deploy files

---
+++ scripts/app.js
@@ -0,0 +1,23 @@
+﻿(function () {
+	var app = angular.module("app", ['ngRoute', 'ngAnimate']);
+	app.config(function ($routeProvider) {
+		$routeProvider
+			 .when('/', {
+			 	templateUrl: 'templates/home.html'
+			 })
+			 .when('/play', {
+			 	templateUrl: 'templates/game.html',
+			 	controller: 'GameController'
+			 })
+			 .when('/about', {
+			 	templateUrl: 'templates/about.html'
+			 })
+			 .otherwise({
+			 	redirectTo: '/'
+			 })
+	});
+
+	app.controller('GameController', function ($scope) {
+		$scope.game = new Game("My Matching Game");
+	});
+})();
