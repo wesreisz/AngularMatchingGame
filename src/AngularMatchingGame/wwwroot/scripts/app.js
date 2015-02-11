cf6a25c2bf23b5f605711875bb2058b3256111a1
Wesley Reisz <wes@wesleyreisz.com>
2015-02-10 23:06:20 -0500

    added ng-animate and ng-routes

--- src/AngularMatchingGame/wwwroot/scripts/app.js
+++ src/AngularMatchingGame/wwwroot/scripts/app.js
@@ -1,5 +1,22 @@
 ﻿(function () {
-	var app = angular.module('app', []);
+	var app = angular.module("app", ['ngRoute', 'ngAnimate']);
+	app.config(function ($routeProvider) {
+		$routeProvider
+			 .when('/', {
+			 	templateUrl: 'templates/home.html'
+			 })
+			 .when('/play', {
+			 	templateUrl: 'templates/game.html',
+			 	controller: 'GameController'
+			 })
+			 .when('/about', {
+			 	templateUrl: 'templates/about.html'
+			 })
+			 .otherwise({
+			 	redirectTo: '/'
+			 })
+	});
+
 	app.controller('GameController', function ($scope) {
 		$scope.game = new Game("My Matching Game");
 	});
